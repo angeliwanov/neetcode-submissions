c9a8c8da26199dc0acb86da0d3e5e9d86e40401e
Angel Ivanov <angel.iwanov@gmail.com>
2026-06-18 12:41:43 +0300

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        size_t left{0};
+        size_t right{height.size()-1};
+        int leftMax{height[left]};
+        int rightMax{height[right]};
+        int water{0};
+
+        while (left < right) {
+            if (leftMax < rightMax) {
+                ++left;
+                leftMax = max(leftMax, height[left]);
+                water += leftMax - height[left];
+            } else {
+                --right;
+                rightMax = max(rightMax, height[right]);
+                water += rightMax - height[right];
+            }
+        }
+
+        return water;
+    }
+};
