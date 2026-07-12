bc244cbb8335406cea1413a38fa3ba3878be0d0d
Angel Ivanov <angel.iwanov@gmail.com>
2026-07-12 08:17:57 +0300

    Add: binary-search-tree-iterator - submission-1

---
+++ Data Structures & Algorithms/binary-search-tree-iterator/submission-1.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+   private:
+    std::vector<int> inorder;
+    size_t idx{0};
+
+   public:
+    BSTIterator(TreeNode* root) { dfs(root); }
+
+    void dfs(TreeNode* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        dfs(node->left);
+        inorder.push_back(node->val);
+        dfs(node->right);
+    }
+
+    int next() { return inorder[idx++]; }
+
+    bool hasNext() { return idx < inorder.size(); }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
