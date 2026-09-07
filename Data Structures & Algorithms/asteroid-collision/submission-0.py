18340cda4e5c6a77bf4f4168ac77a1a8b7b926ba
Angel Ivanov <angel.iwanov@gmail.com>
2026-09-07 10:42:56 +0300

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+
+        for asteroid in asteroids:
+            if asteroid > 0:
+                stack.append(asteroid)
+            else:                
+                while stack and stack[-1] > 0:                                            
+                    if stack[-1] < abs(asteroid):
+                        stack.pop()
+                    elif stack[-1] == abs(asteroid):                        
+                        stack.pop()
+                        asteroid = 0
+                        break
+                    else:
+                        asteroid = 0
+                        break
+                if asteroid:                        
+                    stack.append(asteroid)
+                    
+        return stack
