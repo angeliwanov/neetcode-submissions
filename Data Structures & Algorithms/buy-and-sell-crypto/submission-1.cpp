2e9b791bcbec06f50f362e06e8bb109d9d3ab1cd
Angel Ivanov <angel.iwanov@gmail.com>
2026-09-24 07:43:57 +0300

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int left {0};
+        int profit {0};
+
+        for (int right = 1; right < prices.size(); ++right) {
+            if (prices[right] < prices[left]) {
+                left = right;
+            }
+            profit = max(profit, prices[right] - prices[left]);
+        }
+
+        return profit;
+    }
+};
